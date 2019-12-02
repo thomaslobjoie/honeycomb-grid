af63821ff3a2a8fa5b45cc28ad593c9e6e5b0674
Thomas Lobjoie <thomas.lobjoie@gmail.com>
2019-12-02 23:13:47 +0000

    Tidy up.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
-$( document ).ready(function() {
-    const $honeycombWrapper = $('.honeycomb-wrapper');
-    const $item = $honeycombWrapper.find('.item');
-
+$(document).ready(function() {
     $(window).on('resize', resizeHoneycomb);
     resizeHoneycomb();
-
-    function resizeHoneycomb() {
-        const honeycombColumns = Math.floor($honeycombWrapper.innerWidth() / $item.innerWidth());
-        $honeycombWrapper.attr('data-honeycomb-columns', (honeycombColumns > 5) ? 5 : honeycombColumns);
-    }
 });
+
+function resizeHoneycomb() {
+    const $honeycombWrapper = $('.honeycomb-wrapper');
+    const $item = $honeycombWrapper.find('.item');
+    const honeycombColumns = Math.floor($honeycombWrapper.innerWidth() / $item.innerWidth());
+    $honeycombWrapper.attr('data-honeycomb-columns', (honeycombColumns > 5) ? 5 : honeycombColumns);
+}

058c01a99d94b5cfdc06cf60e5f0aa57df3e1932
Thomas Lobjoie <thomas.lobjoie@gmail.com>
2019-12-02 23:30:42 +0000

    Comment code.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
-$(document).ready(function() {
-    $(window).on('resize', resizeHoneycomb);
-    resizeHoneycomb();
-});
+$(document).ready(resizeHoneycomb);
+
+$(window).on('resize', resizeHoneycomb);
 
 function resizeHoneycomb() {
+    // get the grid wrapper
     const $honeycombWrapper = $('.honeycomb-wrapper');
+    // get the grid item
     const $item = $honeycombWrapper.find('.item');
+    // get number of columns in the grid
     const honeycombColumns = Math.floor($honeycombWrapper.innerWidth() / $item.innerWidth());
+    // update honeycomb columns attribute (limited to 5)
     $honeycombWrapper.attr('data-honeycomb-columns', (honeycombColumns > 5) ? 5 : honeycombColumns);
 }
